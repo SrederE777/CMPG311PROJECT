578c562da73db40c337925cbae3aff8831fa9307
SrederE777 <143420229+SrederE777@users.noreply.github.com>
2024-05-13 01:45:22 +0200

    fixed the employee contact by changing it to employee contract

--- Queries/show_employees.sql
+++ Queries/show_employees.sql
@@ -1,3 +1,3 @@
-SELECT employee_name,employee_surname,contract_job_title AS JobTitle, TRUNC(MONTHS_BETWEEN(SYSDATE, E.EMPLOYEE_DATE_OF_EMPLOYEMENT) / 12) AS NumYears
+SELECT employee_name,employee_surname,contract_job_title AS JobTitle, TRUNC(MONTHS_BETWEEN(SYSDATE, e.employee_date_of_employement) / 12) AS NumYears
 FROM EMPLOYEE e
-JOIN EMPLOYEE_CONTRACT ec ON e.employee_contact_id = ec.employee_contract_id;
+JOIN EMPLOYEE_CONTRACT ec ON e.employee_contract_id = ec.employee_contract_id;
