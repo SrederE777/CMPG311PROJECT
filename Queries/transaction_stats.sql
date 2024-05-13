dd79a7ccd9692f179e5431c5040afc35618533d6
NathanVanR <140237302+NathanVanR@users.noreply.github.com>
2024-05-13 15:10:18 +0200

    tried to merge first two queries

--- Queries/transaction_stats.sql
+++ Queries/transaction_stats.sql
@@ -1,12 +1,12 @@
-SELECT COUNT(*) AS total_transactions,
-       AVG(transaction_total_amount) AS avg_transaction_amount,
-       MAX(transaction_total_amount) AS max_transaction_amount
-FROM TRANSACTIONS;
-
-SELECT COUNT(*) AS NumCafeTransactions, AVG(T.TRANSACTION_TOTAL_AMOUNT)
-FROM CAFE_TRANSACTION ct
+SELECT COUNT(*) AS NumCafeTransactions,
+        AVG(t.transaction_total_amount) AS avg_transaction_amount,
+        MAX(t.transaction_total_amount) AS max_transaction_amount
+        AVG(ct.TRANSACTION_TOTAL_AMOUNT) AS avg_cafe_transaction_amount,
+        MAX(ct.TRANSACTION_TOTAL_AMOUNT) AS max_cafe_transaction_amount
+        FROM CAFE_TRANSACTION ct
 JOIN TRANSACTIONS t on t.TRANSACTION_ID = ct.transaction_id;
 
-SELECT COUNT(*) NumCoworkTransactions, AVG(T.TRANSACTION_TOTAL_AMOUNT)
+
+SELECT COUNT(*) AS NumCoworkTransactions, AVG(T.TRANSACTION_TOTAL_AMOUNT)
 FROM COWORK_TRANSACTION cwt
 JOIN TRANSACTIONS t ON t.TRANSACTION_ID = cwt.TRANSACTION_ID;
